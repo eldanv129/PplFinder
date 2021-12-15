c84ae1249c017f9756a2f2a3ee0fae7a3b030450
eldanv129 <eldanv129@gmail.com>
2021-12-15 09:55:01 +0200

    Country filters working Favorites working re-arranged some code

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import { useHistory } from "react-router";
 
 const NavBar = () => {
-  const [value, setValue] = useState(0);
+  const history = useHistory()
 
   const handleChange = (_e, newValue) => {
-    setValue(newValue);
-  };
+    if(newValue === 1){
+      history.push('/Favorites')
+    }else{
+      history.push('/')
+    }
+  }
 
   return (
     <AppBar position="static" color="transparent" style={{ position: "fixed", top: 0 }}>
       <Tabs
-        value={value}
         onChange={handleChange}
         aria-label="Navigation"
         indicatorColor="primary"
